3da9a6d39ee2b70fbab93c5127338d2e8161fdfe
Moses Kabungo <mose.kabungo@gmail.com>
2024-11-13 14:36:55 +0300

    fix: version mismatches

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 with open("README.md", encoding="utf-8") as fd:
     setup(
         name="agritechtz-pycli",
-        version="1.0.2",
+        version="1.0.3",
         description="A client library for fetching and processing crop price data.",
         long_description=fd.read(),
         long_description_content_type="text/markdown",
