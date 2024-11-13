97a65af75feca6f2edc0f185b664abadd284f1bd
Moses Kabungo <mose.kabungo@gmail.com>
2024-11-13 14:11:40 +0300

    feat: change project version on pypi

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 with open("README.md", encoding="utf-8") as fd:
     setup(
         name="agritechtz-pycli",
-        version="0.1.0",
+        version="1.0.0",
         description="A client library for fetching and processing crop price data.",
         long_description=fd.read(),
         long_description_content_type="text/markdown",
